2df5a79d91f9f38f20b9f246d4e2e7a69fad2d43
Leo Boissard <lsboissard@gmail.com>
2024-08-27 01:47:06 -0300

    add assets files

---
+++ src/assets/index.js
@@ -0,0 +1,53 @@
+import lunar from "./lunar.png";
+import bill from "./bill.png";
+import aline from "./aline.png";
+import card from "./card.png";
+import ford from "./ford.png";
+import dropbox from "./dropbox.png";
+import logo from "./logo.svg";
+import quotes from "./quotes.svg";
+import women from "./women.png";
+import medi from "./medi.svg";
+import mind from "./mind.svg";
+import health from "./health.svg";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import google from "./google.svg";
+import apple from "./apple.svg";
+import arrowUp from "./arrow-up.svg";
+import discount from "./Discount.svg";
+import facebook from "./facebook.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
+import twitter from "./twitter.svg";
+import people01 from "./people01.png";
+import people02 from "./people02.png";
+import people03 from "./people03.png";
+
+export {
+  lunar,
+  bill,
+  aline,
+  card,
+  ford,
+  dropbox,
+  logo,
+  quotes,
+  women,
+  medi,
+  mind,
+  health,
+  menu,
+  close,
+  google,
+  apple,
+  arrowUp,
+  discount,
+  facebook,
+  instagram,
+  linkedin,
+  twitter,
+  people01,
+  people02,
+  people03,
+};
